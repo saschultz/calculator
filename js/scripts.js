554b75b9f8b1d917387b6a30c1cccf36e262af66
Megan Warnock and Sara Schultz <megan@>
2017-02-13 11:34:55 -0800

    add coversion function from celcius to fahrenheit

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,39 @@ var add = function(number1, number2){
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number"));
 
-alert(add(number1, number2));
+//This function subtracts two values
+var subtract = function(number1, number2){
+	return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number"));
+
+//This function multiplies two values
+var multiply = function(number1, number2){
+	return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number"));
+
+//This function divides two values
+var divide = function(number1, number2){
+	return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number"));
+
+var result = subtract(number1, number2);
+alert(result);
+
+//This function converts celcius to fahrenheit
+// var conversion = function(celcius) {
+// 	return ((celcius *=9) /5) +32;
+// };
+//
+// var celcius = parseInt(prompt("Enter the degree in Celcius to convert to Fahrenheit:"));
+//
+// var result = conversion(celcius);
+// alert(result);
